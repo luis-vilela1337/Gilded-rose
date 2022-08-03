3bf27ec75df3336297815cf3d1bb133175344d4c
Luis Fillype Vilela <l.fillype@gmail.com>
2022-08-03 16:58:44 -0300

    chore: project config

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,15 @@
-import { pathsToModuleNameMapper } from  "ts-jest/utils";
+import { pathsToModuleNameMapper } from "ts-jest/utils";
 const { compilerOptions } = require("./tsconfig");
 
 export default {
-  roots: ['<rootDir>/app', '<rootDir>/test/jest'],
+  roots: ["<rootDir>/app", "<rootDir>/test/jest"],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
   },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' } ),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
